e45f62acd1e7aaa57a9c3e5741ebb1088fb73822
apostolis anastasiou <apostolos.anastasiou@e-food.gr>
2024-10-21 13:13:24 +0300

    remove redundant code in bench

--- benchmarks/i-eat-parent-chili.js
+++ benchmarks/i-eat-parent-chili.js
@@ -1,127 +1,53 @@
-
-const opts = {
-  showBadge: true,
-  showPrice: true,
-  showPriceDiscount: true,
-  showPriceFull: true,
-  showPriceMode: true,
-  showPriceSize: true,
-  showPriceMetric: true,
-  showTitle: true,
-  showDescription: true,
-  showLogo: true,
-  showRestaurantId: true,
-  showImages: true
-};
-
-const generateOfferBadgeSimple = (opts, offer) => {
-  let badge = '';
-  if (opts.showBadge) {
-    badge = offer.badge;
-  }
-  return badge;
-};
+// findings:
+// - removing `excludeTags`
+//   - forEachWay: stays same
+//   - forWay: decreases ms/ops by 10ms (50 -> 40)
+//
+// - having more complex objects in getOffers
+//   - forEachWay: stays same
+//   - forWay: stays same
+//
+// - terms seems to be the only reason any of these two
+//   perform slow. Removing terms (foreach and for clauses)
+//   - forEachWay: 9,384 ops & 0.1017 ms/ops
+//   - forWay: 36,889 ops & 0.02711 ms/ops
+//
+// - terms: When I change the term's title to `title+${i}`
+//  - forEachWay: slows it down by 1ms/op (28.03 -> 29.03)
+//  - forWay: makes it faster by 10ms/op (50ms -> 40ms)
 
 const populateData = (searchEntries, excludeTags, terms) => {
   const se = (i) => ({
-    isOpen: () => true,
     getOffers: () => [
-      {
-        id: i,
-        title: 'title',
-        description: 'description',
-        tag: 'tag',
-        metric_description: 'metric_description',
-        size_info: 'size_info',
-        full_price: 'full_price',
-        mode: 'mode',
-        price: 'price',
-        logo: 'logo',
-        restaurant_id: i,
-        badge: 'badge',
-        images: {
-          menu: null,
-          menu_bh: null
-        }
-      }
+      i
     ],
-    getId: () => i
   });
   for (let i = 0; i < 10000; i++) {
     const seInstance = se(i);
     searchEntries.push(seInstance);
     excludeTags.push('tag' + i);
-    terms.push('title' + i);
+    terms.push('title'+i);
   }
 };
 
-const forEachWay = (searchEntries, excludeTags, terms, validOffers, validRestaurantsIds) => {
-  searchEntries.forEach((se, sid) => {
-    if (!se.isOpen()) {return;}
+const forEachWay = (searchEntries, excludeTags, terms) => {
+  searchEntries.forEach((se) => {
     const offers = se.getOffers([], excludeTags);
     offers.forEach((o) => {
       terms.forEach((t) => {
-        if (o.titleSlug?.includes(t) || o.descriptionSlug?.includes(t)) {
-          const offer = {
-            id:o.id,
-            title:o.title,
-            description:o.description,
-            tag:o.tag,
-            metric_description:o.metric_description,
-            size_info:o.size_info,
-            full_price:o.full_price,
-            mode:o.mode,
-            price:o.price,
-            logo:o.logo,
-            restaurant_id: se.getId(),
-            badge: generateOfferBadgeSimple(opts, o),
-            images: {
-              menu: null,
-              menu_bh: null
-            }
-          };
-          validOffers.push(offer);
-          validRestaurantsIds.push(sid);
-        }
+        // something here
       });
     });
   });
 };
 
-const forWay = (searchEntries, excludeTags, terms, validOffers, validRestaurantsIds) => {
+const forWay = (searchEntries, excludeTags, terms) => {
   for (let sid = 0; sid < searchEntries.length; sid++) {
     const se = searchEntries[sid];
-    if (!se.isOpen()) {
-      continue;
-    }
-  
     const offers = se.getOffers([], excludeTags);
     for (let i = 0; i < offers.length; i++) {
-      const o = offers[i];
       for (let j = 0; j < terms.length; j++) {
-        const t = terms[j];
-        if (o.titleSlug?.includes(t) || o.descriptionSlug?.includes(t)) {
-          const offer = {
-            id: o.id,
-            title: o.title,
-            description: o.description,
-            tag: o.tag,
-            metric_description: o.metric_description,
-            size_info: o.size_info,
-            full_price: o.full_price,
-            mode: o.mode,
-            price: o.price,
-            logo: o.logo,
-            restaurant_id: se.getId(),
-            badge: generateOfferBadgeSimple(opts, o),
-            images: {
-              menu: null,
-              menu_bh: null
-            }
-          };
-          validOffers.push(offer);
-          validRestaurantsIds.push(sid);
-        }
+        // something here
       }
     }
   }
@@ -138,14 +64,12 @@ export default {
         let searchEntries = [];
         let excludeTags = [];
         let terms = [];
-        const validOffers = [];
-        const validRestaurantsIds = [];
         populateData(searchEntries, excludeTags, terms)
 
         return () => {
           let result = 0
           for (let i = 0; i < iterations; i++) {
-            forEachWay(searchEntries, excludeTags, terms, validOffers, validRestaurantsIds);
+            forEachWay(searchEntries, excludeTags, terms);
           }
           return result
         }
@@ -157,14 +81,12 @@ export default {
         let searchEntries = [];
         let excludeTags = [];
         let terms = [];
-        const validOffers = [];
-        const validRestaurantsIds = [];
         populateData(searchEntries, excludeTags, terms)
 
         return () => {
           let result = 0
           for (let i = 0; i < iterations; i++) {
-            forWay(searchEntries, excludeTags, terms, validOffers, validRestaurantsIds);
+            forWay(searchEntries, excludeTags, terms);
           }
           return result
         }
